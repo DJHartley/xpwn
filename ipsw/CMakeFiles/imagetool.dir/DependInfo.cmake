95683f31459b2ba323c89a7201013a7b2404c301
Grant Paul <chpwn@chpwn.com>
2010-10-25 22:14:45 -0700

    Began restructure and removal of unneeded tools.

---
+++ ipsw/CMakeFiles/imagetool.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/xuzz/code/xpwn/ipsw-patch/imagetool.c" "/home/xuzz/code/xpwn/ipsw-patch/CMakeFiles/imagetool.dir/imagetool.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xuzz/code/xpwn/ipsw-patch/CMakeFiles/xpwn.dir/DependInfo.cmake"
+  "/home/xuzz/code/xpwn/dmg/CMakeFiles/dmg.dir/DependInfo.cmake"
+  "/home/xuzz/code/xpwn/hfs/CMakeFiles/hfs.dir/DependInfo.cmake"
+  "/home/xuzz/code/xpwn/common/CMakeFiles/common.dir/DependInfo.cmake"
+  "/home/xuzz/code/xpwn/minizip/CMakeFiles/minizip.dir/DependInfo.cmake"
+  )
